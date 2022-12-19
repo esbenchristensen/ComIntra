b50a954e6c3d8b6e05181bd1fdfd15284eac7870
Esben Christensen <80604101+esbenvh@users.noreply.github.com>
2022-12-19 14:03:31 +0100

    Delete Comintra directory

--- Comintra/comintra-script.js
+++
@@ -1,8 +0,0 @@
-/* De ansatte glemmer ofte deres passwords og skal hele tiden gå op til IT-afdelingen 
-og få nulstillet det. Derfor vil de gerne have en online portal, hvor man kan 
-få nulstillet ens adgangskode, ved brug af sit unikke ID. Man skal skrive ens ID 
-og få en besked tilbage om at der er sendt en mail til den e-mail, som ID-passer på. 
-(Det er vigtigt at mailen bliver skrevet ud fx “Tak Karsten Birch- ID3201, 
-    der er sendt en mail til Kabi@Virksomhed.dk, med link til at nulstille din kode”) */
-
-
