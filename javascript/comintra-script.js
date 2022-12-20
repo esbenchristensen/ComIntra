603e32eead1074c05bf2c07c667b0dcdad8933c9
Esben Christensen <80604101+esbenvh@users.noreply.github.com>
2022-12-20 11:30:36 +0100

    Add files via upload

---
+++ javascript/comintra-script.js
@@ -0,0 +1,63 @@
+let usernames = [
+        'esben',
+        'Freja',
+        'Oliver',
+        'Mathias'
+      ];
+
+let emails = [
+        'esben@gmail.com',
+        'Freja@gmail.com',
+        'Oliver@gmail.com',
+        'Mathias@gmail.com'
+      ];
+
+let names = [
+        'Esben Christensen',
+        'Freja Pedersen',
+        'Oliver Larsen',
+        'Mathias Steenberg'
+      ];
+
+
+function redirect() {
+        window.location.pathname = ("/HTML/oversigt.html");
+      }
+
+
+function login() {
+
+        let idinput = document.getElementById('idinput').value.toLowerCase();
+        localStorage.setItem("idinput", idinput);
+
+
+        for (let i = 0; i < usernames.length; i++) {
+            if (idinput === usernames[i]) {
+                redirect();
+                alert("Logger ind!");
+                break;
+            }
+        }
+    }
+
+
+let activeUserId = localStorage.getItem("idinput");
+
+let activeUserPlace = (usernames.indexOf(activeUserId));
+
+let activeUserEmail = (emails.at(activeUserPlace));
+
+let activeUserName = (names.at(activeUserPlace));
+
+let activeUserInfo = activeUserId + " " + activeUserEmail + " " + activeUserName;
+
+console.log(activeUserInfo)
+
+
+document.getElementById("activeUserName").innerHTML = activeUserName;
+
+
+function passwordReset() {
+  document.getElementById("passwordIsReset").innerHTML = activeUserName + " der er sendt et nulstillingslink til din mail " + activeUserEmail;
+}
+
