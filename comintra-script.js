375a2d9de9d66295ed8fd0c36dbc91699b272de6
Esben Christensen <80604101+esbenvh@users.noreply.github.com>
2022-12-22 12:30:12 +0100

    updated email function

--- comintra-script.js
+++ comintra-script.js
@@ -6,7 +6,7 @@ let usernames = [
       ];
 
 let emails = [
-        'esben@gmail.com',
+        'esbenvh@gmail.com',
         'Freja@gmail.com',
         'Oliver@gmail.com',
         'Mathias@gmail.com'
@@ -61,4 +61,19 @@ document.getElementById("activeUserName").innerHTML = activeUserName;
 function passwordReset() {
   document.getElementById("passwordIsReset").innerHTML = activeUserName + ", der er sendt et nulstillingslink til din mail " + activeUserEmail + ".";
 }
+
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "esbenvh@gmail.com",
+        Password : "E2D3B0D4A60B668E02B90670B485A60DFCA9",
+        To : activeUserEmail,
+        From : "esbenvh@gmail.com",
+        Subject : "Test email",
+        Body : "<h1>ComIntra</h1><h2><strong>Nulstil email</strong></h2><p>Klik på knappen for at nulstill din kode</p><button>Nulstil kode</button>"
+    }).then(
+      message => alert("Der er sendt et nulstillingslink til din email.")
+    );
+    }
 
--- mail.js
+++ mail.js
@@ -1,13 +1,19 @@
+let mail = "esbenchristensen9900@gmail.com"
+
+
+
 function sendEmail() {
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "esbenvh@gmail.com",
         Password : "E2D3B0D4A60B668E02B90670B485A60DFCA9",
-        To : 'esbenvh@gmail.com',
+        To : mail,
         From : "esbenvh@gmail.com",
         Subject : "Test email",
-        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+        Body : "<h1>ComIntra</h1><h2><strong>Nulstil email</strong></h2><p>Klik på knappen for at nulstill din kode</p><button>Nulstil kode</button>"
     }).then(
-      message => alert(message)
+      message => alert("Der er sendt et nulstillingslink til din email.")
     );
     }
+
+    
