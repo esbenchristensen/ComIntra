9a7a781bc34d0d0d6159369464434461e118eca7
Esben Christensen <80604101+esbenvh@users.noreply.github.com>
2023-01-04 08:13:26 +0100

    updated login info for email api

--- comintra-script.js
+++ comintra-script.js
@@ -66,10 +66,10 @@ function passwordReset() {
 function sendEmail() {
     Email.send({
         Host : "smtp.elasticemail.com",
-        Username : "esbenvh@gmail.com",
-        Password : "E2D3B0D4A60B668E02B90670B485A60DFCA9",
+        Username : "esbenchristensen9900@gmail.com",
+        Password : "3A69440A5768CE04C3BACBE99014E3309988",
         To : activeUserEmail,
-        From : "esbenvh@gmail.com",
+        From : "esbenchristensen9900@gmail.com",
         Subject : "Test email",
         Body : "<h1>ComIntra</h1><h2><strong>Nulstil email</strong></h2><p>Klik på knappen for at nulstill din kode</p><button>Nulstil kode</button>"
     }).then(
